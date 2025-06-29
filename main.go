8f53e74ef5cb74892588b40eb55e3045cb95439c
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-06-29 18:10:33 -0400

    feat: add nginx config and container

--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 || os.Args[1] == "" {
-		log.Fatalln("Listening port should be provided")
-	}
-
-	port := os.Args[1]
-
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(
-			fmt.Sprintf(
-				"Hello world from app in port %s", port,
-			),
-		))
+		w.Write([]byte("Hello world!"))
 	})
 
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	http.ListenAndServe("", nil)
 }
