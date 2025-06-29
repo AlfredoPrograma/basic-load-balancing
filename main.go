ee8b966693d02278e31c05a12b289f410dabb96a
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-06-29 16:28:32 -0400

    feat: implement basic http server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		log.Fatalln("Listening port should be provided")
+	}
+
+	port := os.Args[1]
+
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(
+			fmt.Sprintf(
+				"Hello world from app in port %s", port,
+			),
+		))
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
